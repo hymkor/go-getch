597deeb9da37e61ea09db3e4fa5fb403cfa1d0cb
Kaoru HAYAMA <iyahaya@nifty.com>
2017-03-14 15:42:58 +0900

    Wrote test for Wait

--- getch_test.go
+++ getch_test.go
@@ -6,6 +6,30 @@ import (
 	"time"
 )
 
+func reportEvent(e Event){
+	if k := e.Key; k != nil {
+		fmt.Printf("key down: code=%04X scan=%04X shift=%04X\n",
+			k.Rune, k.Scan, k.Shift)
+	}
+	if k := e.KeyUp ; k != nil {
+		fmt.Printf("key up: code=%04X scan=%04X shift=%04X\n",
+			k.Rune, k.Scan, k.Shift)
+	}
+	if r := e.Resize; r != nil {
+		fmt.Printf("window resize: width=%d height=%d\n",
+			r.Width, r.Height)
+	}
+	if e.Mouse != nil {
+		fmt.Println("mouse event")
+	}
+	if e.Menu != nil {
+		fmt.Println("menu event")
+	}
+	if e.Focus != nil {
+		fmt.Println("focus event")
+	}
+}
+
 func TestAll(t *testing.T) {
 	if err := Flush(); err != nil {
 		t.Error(err.Error())
@@ -13,16 +37,7 @@ func TestAll(t *testing.T) {
 	}
 	for i := 0; i < 3; i++ {
 		fmt.Printf("[%d/3] ", i+1)
-		e := All()
-		if k := e.Key; k != nil {
-			fmt.Printf("key hit: code=%04X scan=%04X shift=%04X\n",
-				k.Rune, k.Scan, k.Shift)
-		}
-		if r := e.Resize; r != nil {
-			fmt.Printf("window resize: width=%d height=%d\n",
-				r.Width, r.Height)
-		}
-
+		reportEvent(All())
 	}
 }
 
@@ -46,20 +61,25 @@ func TestCount(t *testing.T) {
 		time.Sleep(time.Second)
 	}
 	fmt.Printf("break(n=%d)\n", n)
-	e := All()
-	if e.Key != nil {
-		fmt.Printf("'%c' typed.\n", e.Key.Rune)
-	} else if e.Resize != nil {
-		fmt.Printf("(%d,%d)\n", e.Resize.Width, e.Resize.Height)
-	} else if e.Mouse != nil {
-		fmt.Println("mouse event")
-	} else if e.Menu != nil {
-		fmt.Println("menu event")
-	} else if e.Focus != nil {
-		fmt.Println("focus event")
-	} else if e.KeyUp != nil {
-		fmt.Printf("keyup event '%c'\n",e.KeyUp.Rune)
-	} else {
-		fmt.Printf("Otherwise\n")
+	reportEvent(All())
+}
+
+func TestWait(t *testing.T) {
+	var err error
+	if err = Flush(); err != nil {
+		t.Error(err.Error())
+		return
 	}
+	for {
+		hit, hit_err := Wait(1000)
+		if hit_err != nil {
+			t.Error(hit_err.Error())
+			return
+		}
+		if hit {
+			break
+		}
+		fmt.Println("Nothing")
+	}
+	reportEvent(All())
 }
