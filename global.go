0ab5bb1d53d82f7fb9a9ed41f500fdf48da8e260
Kaoru HAYAMA <iyahaya@nifty.com>
2017-06-09 01:29:27 +0900

    init() -> lazyinit() and call it at calling public functions at first

--- global.go
+++ global.go
@@ -2,7 +2,10 @@ package getch
 
 var hconin *Handle
 
-func init() {
+func lazyinit() {
+	if hconin != nil {
+		return
+	}
 	var err error
 	hconin, err = New()
 	if err != nil {
@@ -12,39 +15,48 @@ func init() {
 
 // Get all console-event (keyboard,resize,...)
 func All() Event {
+	lazyinit()
 	return hconin.All()
 }
 
 // Get character as a Rune
 func Rune() rune {
+	lazyinit()
 	return hconin.Rune()
 }
 
 func Count() (int, error) {
+	lazyinit()
 	return hconin.GetNumberOfEvent()
 }
 
 func Flush() error {
+	lazyinit()
 	return hconin.Flush()
 }
 
 // wait for keyboard event
 func Wait(timeout_msec uintptr) (bool, error) {
+	lazyinit()
 	return hconin.Wait(timeout_msec)
 }
 
 func Within(msec uintptr) (Event, error) {
+	lazyinit()
 	return hconin.Within(msec)
 }
 
 func RuneWithin(msec uintptr) (rune, error) {
+	lazyinit()
 	return hconin.RuneWithin(msec)
 }
 
 func IsCtrlCPressed() bool {
+	lazyinit()
 	return hconin.IsCtrlCPressed()
 }
 
 func DisableCtrlC() {
+	lazyinit()
 	hconin.DisableCtrlC()
 }
